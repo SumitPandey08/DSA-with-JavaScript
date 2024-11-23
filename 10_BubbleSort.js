3984d33784109e83caa92d2dcc2cb39971f64b25
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-11-23 12:26:33 +0530

    Decending BubbleSort with ascending and Time complexity

--- 10_BubbleSort.js
+++ 10_BubbleSort.js
@@ -20,3 +20,30 @@ function BubbleSort(array){
 
 console.log(BubbleSort([8 , 20 , -2 , 4 , -6]));
 //Big-O = O(n^2) because there is nested loop which increase time complexity by n^2
+
+
+
+
+//Now Decending 
+function DecendingBubbleSort(array){
+    let swapped  ;
+
+    do{
+    for(i = 0 ; i < array.length - 1 ; i++){
+        swapped = false
+        if(array[i]  < array[i+1]){
+            temp = array[i+1]
+            array[i+1] = array[i]
+            array[i] = temp
+            swapped = true
+        }
+        else{
+            continue ;
+        }
+    }}while(swapped)
+    return array ;
+
+
+}
+console.log(DecendingBubbleSort([8 , 20 , -2 , 4 , -6]));
+//Big-O = O(n^2) because there is nested loop which increase time complexity by n^2
