f8745a13d2ffc95c737cf99518f0743779fde001
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-12-01 22:17:33 +0530

    insertion sort with time complexity

--- 11_InsertionSort.js
+++ 11_InsertionSort.js
@@ -14,3 +14,18 @@ return array ;
 console.log(InsertionSort([-2 , -11 , -34 , 4 , 54, 9 ]));
 
 // Big-O  = O(n^2)  because of nested loop
+var insertionSortList = function(head) {
+    for(let i = 1; i < head.length; i++){
+     let numberToInsert = head[i] ;
+     let j = i -1 ;
+     while(j >= 0 && head[j] > numberToInsert){
+        head[j+1] = head[j]
+        j-- ;
+    }
+    head[j+1] = numberToInsert ;
+}
+return head ;
+
+};
+
+console.log(insertionSortList([0 , 1 ,2, 4 , -3 , -5 , -10]));
