f5fb3965ee9e4a57b3f289855e856c9c13535b7a
SumitPandey08 <sp.sumitpandey06@gmail.com>
2024-12-01 22:18:57 +0530

    send the single ocureence in a array

---
+++ LeetCodes/SendTheSIngleOccurence.js
@@ -0,0 +1,13 @@
+function SingleOccurance(arr){
+ if(arr.length == 1){
+        return arr[0];
+
+    }
+    for(i = 0 ; i < arr.length ; i++){
+        if(arr.contain(arr[0])){
+           }
+    }
+
+
+
+}
